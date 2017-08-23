bf001be62b184b61c6147e3b02b01297992c4423
iClaude <agostini.claudio1@gmail.com>
2017-08-23 21:10:23 +0200

    NDC - working on add scheduled recording activity 2

--- app/src/main/java/com/danielkim/soundrecorder/activities/AddScheduledRecordingActivity.java
+++ app/src/main/java/com/danielkim/soundrecorder/activities/AddScheduledRecordingActivity.java
@@ -4,12 +4,19 @@
 
 package com.danielkim.soundrecorder.activities;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import com.danielkim.soundrecorder.R;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * Activity used to add a new scheduled recording.
@@ -20,6 +27,14 @@ public class AddScheduledRecordingActivity extends AppCompatActivity {
 
     private TextView tvDateStart;
     private TextView tvDateEnd;
+    private TextView tvTimeStart;
+    private TextView tvTimeEnd;
+
+    private DateFormat dateFormat = new SimpleDateFormat("EEE d MMM yyyy");
+    private DateFormat timeFormat = new SimpleDateFormat("HH:mm");
+    private int yearStart, monthStart, dayStart, hourStart, minuteStart;
+    private int yearEnd, monthEnd, dayEnd, hourEnd, minuteEnd;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,9 +43,35 @@ public class AddScheduledRecordingActivity extends AppCompatActivity {
 
         tvDateStart = (TextView) findViewById(R.id.tvDateStart);
         tvDateEnd = (TextView) findViewById(R.id.tvDateEnd);
-
+        tvTimeStart = (TextView) findViewById(R.id.tvTimeStart);
+        tvTimeEnd = (TextView) findViewById(R.id.tvTimeEnd);
 
         long selectedDateLong = getIntent().getLongExtra(EXTRA_DATE_LONG, System.currentTimeMillis());
-
+        tvDateStart.setText(dateFormat.format(new Date(selectedDateLong)));
+        tvDateEnd.setText(dateFormat.format(new Date(selectedDateLong)));
+        tvTimeStart.setText(timeFormat.format(new Date(System.currentTimeMillis())));
+        tvTimeEnd.setText(timeFormat.format(new Date(System.currentTimeMillis() + 1000 * 60 * 60)));
     }
+
+    // Listener of DatePickerDialog for start date.
+    DatePickerDialog.OnDateSetListener dateStartListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            yearStart = year;
+            monthStart = month;
+            dayStart = dayOfMonth;
+            tvDateStart.setText(dateFormat.format(new GregorianCalendar(year, month, dayOfMonth).getTime()));
+        }
+    };
+
+    // Listener of DatePickerDialog for end date.
+    DatePickerDialog.OnDateSetListener dateEndListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            yearEnd = year;
+            monthEnd = month;
+            dayEnd = dayOfMonth;
+            tvDateEnd.setText(dateFormat.format(new GregorianCalendar(year, month, dayOfMonth).getTime()));
+        }
+    };
 }
