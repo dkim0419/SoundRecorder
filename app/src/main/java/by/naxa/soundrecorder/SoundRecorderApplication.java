c3b61900ee3db52157b64676b5c80b51298e80ac
naXa! <naxa@nextmail.ru>
2018-09-06 20:39:51 +0300

    Add LeakCanary - a memory leak detection library

--- app/src/main/java/by/naxa/soundrecorder/SoundRecorderApplication.java
+++ app/src/main/java/by/naxa/soundrecorder/SoundRecorderApplication.java
@@ -3,6 +3,7 @@ package by.naxa.soundrecorder;
 import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
+import com.squareup.leakcanary.LeakCanary;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -11,6 +12,12 @@ public class SoundRecorderApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
         if (BuildConfig.USE_CRASHLYTICS) {
             final Fabric fabric = new Fabric.Builder(this)
                     .kits(new Crashlytics())
