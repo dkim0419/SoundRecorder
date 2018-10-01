6e476769100e522ab56b9cfcd7203938d4f6ee2a
prasad <prasadbankar1997@gmail.com>
2018-10-01 20:20:37 +0530

    Target sdk 28

--- app/src/main/java/com/danielkim/soundrecorder/activities/SettingsActivity.java
+++ app/src/main/java/com/danielkim/soundrecorder/activities/SettingsActivity.java
@@ -7,6 +7,7 @@ import android.preference.PreferenceActivity;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.danielkim.soundrecorder.R;
@@ -16,7 +17,7 @@ import com.danielkim.soundrecorder.fragments.SettingsFragment;
  * Created by Daniel on 5/22/2017.
  */
 
-public class SettingsActivity extends android.support.v7.app.ActionBarActivity {
+public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
