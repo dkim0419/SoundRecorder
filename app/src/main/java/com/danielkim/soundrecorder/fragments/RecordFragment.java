a062aaed1b23a5f8fe25305437c7dd7ca4232786
sJvs <jibesh@shrestha.net.np>
2019-04-28 16:05:01 +1200

    Permission check for RECORD_AUTIO and WRITE_EXTERNAL_STORAGE, Temporarily removed, pause and resume functionaity

--- app/src/main/java/com/danielkim/soundrecorder/fragments/RecordFragment.java
+++ app/src/main/java/com/danielkim/soundrecorder/fragments/RecordFragment.java
@@ -1,11 +1,18 @@
 package com.danielkim.soundrecorder.fragments;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.SystemClock;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,17 +28,13 @@ import com.melnykov.fab.FloatingActionButton;
 
 import java.io.File;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * to handle interaction events.
- * Use the {@link RecordFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class RecordFragment extends Fragment {
+public class RecordFragment extends Fragment implements View.OnClickListener {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_POSITION = "position";
     private static final String LOG_TAG = RecordFragment.class.getSimpleName();
+    private static final int PERMISSIONS_REQUEST_RECORD_AUDIO_AND_WRITE_EXTERNAL = 11069;
+    private static final int PERMISSIONS_REQUEST_RECORD_AUDIO = 11070;
+    private static final int PERMISSIONS_REQUEST_WRITE_EXTERNAL = 11071;
 
     private int position;
 
@@ -42,8 +45,9 @@ public class RecordFragment extends Fragment {
     private TextView mRecordingPrompt;
     private int mRecordPromptCount = 0;
 
-    private boolean mStartRecording = true;
-    private boolean mPauseRecording = true;
+    private boolean isRecording = false;
+    private boolean isPaused = false;
+    private Intent intent;
 
     private Chronometer mChronometer = null;
     long timeWhenPaused = 0; //stores time when user clicks pause button
@@ -84,34 +88,47 @@ public class RecordFragment extends Fragment {
         mRecordButton = recordView.findViewById(R.id.btnRecord);
         mRecordButton.setColorNormal(getResources().getColor(R.color.primary));
         mRecordButton.setColorPressed(getResources().getColor(R.color.primary_dark));
-        mRecordButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onRecord(mStartRecording);
-                mStartRecording = !mStartRecording;
-            }
-        });
+        mRecordButton.setOnClickListener(this);
 
         mPauseButton = recordView.findViewById(R.id.btnPause);
         mPauseButton.setVisibility(View.GONE); //hide pause button before recording starts
-        mPauseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onPauseRecord(mPauseRecording);
-                mPauseRecording = !mPauseRecording;
-            }
-        });
+        mPauseButton.setOnClickListener(this);
 
         return recordView;
     }
 
-    // Recording Start/Stop
-    //TODO: recording pause
-    private void onRecord(boolean start){
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnRecord:
+                if(isRecording){
+                    stopRecording();
+                }else {
+                   startRecording();
+                }
+                break;
 
-        Intent intent = new Intent(getActivity(), RecordingService.class);
+            case R.id.btnPause:
+                break;
+        }
+    }
 
-        if (start) {
+
+    private void startRecording(){
+
+        if ((ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.RECORD_AUDIO)!= PackageManager.PERMISSION_GRANTED)&&
+                (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)) {
+            requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_RECORD_AUDIO_AND_WRITE_EXTERNAL);
+        }else if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.RECORD_AUDIO)!= PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, PERMISSIONS_REQUEST_RECORD_AUDIO);
+        }else if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_WRITE_EXTERNAL);
+        }else{
+            Log.i("RECORD","STARTED");
+            if(intent == null){
+                intent = new Intent(getContext(),RecordingService.class);
+            }
+
             // start recording
             mRecordButton.setImageResource(R.drawable.ic_media_stop);
             //mPauseButton.setVisibility(View.VISIBLE);
@@ -148,38 +165,68 @@ public class RecordFragment extends Fragment {
 
             mRecordingPrompt.setText(getString(R.string.record_in_progress) + ".");
             mRecordPromptCount++;
-
-        } else {
-            //stop recording
-            mRecordButton.setImageResource(R.drawable.ic_mic_white_36dp);
-            //mPauseButton.setVisibility(View.GONE);
-            mChronometer.stop();
-            mChronometer.setBase(SystemClock.elapsedRealtime());
-            timeWhenPaused = 0;
-            mRecordingPrompt.setText(getString(R.string.record_prompt));
-
-            getActivity().stopService(intent);
-            //allow the screen to turn off again once recording is finished
-            getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            isRecording = true;
         }
+
+
+    }
+    private void stopRecording(){
+        //stop recording
+        mRecordButton.setImageResource(R.drawable.ic_mic_white_36dp);
+        //mPauseButton.setVisibility(View.GONE);
+        mChronometer.stop();
+        mChronometer.setBase(SystemClock.elapsedRealtime());
+        timeWhenPaused = 0;
+        mRecordingPrompt.setText(getString(R.string.record_prompt));
+
+        getActivity().stopService(intent);
+        //allow the screen to turn off again once recording is finished
+        getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        isRecording = false;
     }
 
-    //TODO: implement pause recording
-    private void onPauseRecord(boolean pause) {
-        if (pause) {
-            //pause recording
-            mPauseButton.setCompoundDrawablesWithIntrinsicBounds
-                    (R.drawable.ic_media_play ,0 ,0 ,0);
-            mRecordingPrompt.setText(getString(R.string.resume_recording_button).toUpperCase());
-            timeWhenPaused = mChronometer.getBase() - SystemClock.elapsedRealtime();
-            mChronometer.stop();
-        } else {
-            //resume recording
-            mPauseButton.setCompoundDrawablesWithIntrinsicBounds
-                    (R.drawable.ic_media_pause ,0 ,0 ,0);
-            mRecordingPrompt.setText(getString(R.string.pause_recording_button).toUpperCase());
-            mChronometer.setBase(SystemClock.elapsedRealtime() + timeWhenPaused);
-            mChronometer.start();
+    private void pauseRecording(){
+        //pause recording
+        mPauseButton.setCompoundDrawablesWithIntrinsicBounds
+                (R.drawable.ic_media_play ,0 ,0 ,0);
+        mRecordingPrompt.setText(getString(R.string.resume_recording_button).toUpperCase());
+        timeWhenPaused = mChronometer.getBase() - SystemClock.elapsedRealtime();
+        mChronometer.stop();
+    }
+
+    private void resumeRecording(){
+        //resume recording
+        mPauseButton.setCompoundDrawablesWithIntrinsicBounds
+                (R.drawable.ic_media_pause ,0 ,0 ,0);
+        mRecordingPrompt.setText(getString(R.string.pause_recording_button).toUpperCase());
+        mChronometer.setBase(SystemClock.elapsedRealtime() + timeWhenPaused);
+        mChronometer.start();
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+
+            case PERMISSIONS_REQUEST_RECORD_AUDIO_AND_WRITE_EXTERNAL:
+
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                    startRecording();
+                }
+                break;
+            case PERMISSIONS_REQUEST_RECORD_AUDIO:
+
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    startRecording();
+                }
+                break;
+            case PERMISSIONS_REQUEST_WRITE_EXTERNAL:
+
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    startRecording();
+                }
+                break;
         }
     }
 }
