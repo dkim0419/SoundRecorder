4f6f83d99db607128db16410e40025dc3221a0c5
DarkRider080 <pietrovit97@gmail.com>
2020-11-26 16:08:58 +0100

    Add text area in progress

---
+++ app/src/main/java/com/danielkim/soundrecorder/CustomAlertDialogForExtractedText.java
@@ -0,0 +1,4 @@
+package com.danielkim.soundrecorder;
+
+public class CustomAlertDialogForExtractedText {
+}
--- app/src/main/java/com/danielkim/soundrecorder/activities/MainActivity.java
+++ app/src/main/java/com/danielkim/soundrecorder/activities/MainActivity.java
@@ -102,4 +102,6 @@ public class MainActivity extends ActionBarActivity{
 
     public MainActivity() {
     }
+
+
 }
