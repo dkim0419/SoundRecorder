d5b49e5d84957f9a5b68c27bf132ec3a15327b9c
DarkRider080 <pietrovit97@gmail.com>
2020-11-30 19:45:35 +0100

    Text Extraction Refinement

--- app/src/main/java/com/danielkim/soundrecorder/adapters/FileViewerAdapter.java
+++ app/src/main/java/com/danielkim/soundrecorder/adapters/FileViewerAdapter.java
@@ -31,6 +31,7 @@ import com.ibm.cloud.sdk.core.security.IamAuthenticator;
 import com.ibm.watson.speech_to_text.v1.SpeechToText;
 import com.ibm.watson.speech_to_text.v1.model.RecognizeOptions;
 import com.ibm.watson.speech_to_text.v1.model.SpeechRecognitionAlternative;
+import com.ibm.watson.speech_to_text.v1.model.SpeechRecognitionResult;
 import com.ibm.watson.speech_to_text.v1.model.SpeechRecognitionResults;
 import com.ibm.watson.speech_to_text.v1.websocket.BaseRecognizeCallback;
 
@@ -65,7 +66,6 @@ public class FileViewerAdapter extends RecyclerView.Adapter<FileViewerAdapter.Re
 
     @Override
     public void onBindViewHolder(final RecordingsViewHolder holder, int position) {
-
         item = getItem(position);
         long itemDuration = item.getLength();
 
@@ -86,24 +86,29 @@ public class FileViewerAdapter extends RecyclerView.Adapter<FileViewerAdapter.Re
         holder.cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    PlaybackFragment playbackFragment = new PlaybackFragment().newInstance(getItem(holder.getPosition()));
-                    FragmentTransaction transaction = ((FragmentActivity) mContext)
-                            .getSupportFragmentManager()
-                            .beginTransaction();
+                int position = holder.getPosition();
+                File file = new File(getItem(position).getFilePath());
 
-                    playbackFragment.show(transaction, "dialog_playback");
+                if (file.exists()){
+                    try {
+                        PlaybackFragment playbackFragment = new PlaybackFragment().newInstance(getItem(holder.getPosition()));
+                        FragmentTransaction transaction = ((FragmentActivity) mContext)
+                                .getSupportFragmentManager()
+                                .beginTransaction();
 
-                } catch (Exception e) {
-                    Log.e(LOG_TAG, "exception", e);
+                        playbackFragment.show(transaction, "dialog_playback");
+
+                    } catch (Exception e) {
+                        Log.e(LOG_TAG, "exception", e);
+                    }
                 }
+                else Toast.makeText(mContext, mContext.getResources().getString(R.string.toast_file_does_not_exist), Toast.LENGTH_LONG).show();
             }
         });
 
         holder.cardView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-
                 final ArrayList<String> entrys = new ArrayList<String>();
                 entrys.add(mContext.getString(R.string.dialog_file_convert));
                 entrys.add(mContext.getString(R.string.dialog_file_share));
@@ -165,7 +170,12 @@ public class FileViewerAdapter extends RecyclerView.Adapter<FileViewerAdapter.Re
                             }
                         }
                         else if (item == 2) {
-                            renameFileDialog(holder.getPosition());
+                            try {
+                                renameFileDialog(holder.getPosition());
+                            }
+                            catch (FileNotFoundException e){
+                                Toast.makeText(mContext, mContext.getResources().getString(R.string.toast_file_does_not_exist), Toast.LENGTH_LONG).show();
+                            }
                         }
                         else if (item == 3) {
                             deleteFileDialog(holder.getPosition());
@@ -195,12 +205,14 @@ public class FileViewerAdapter extends RecyclerView.Adapter<FileViewerAdapter.Re
         speechToText.setServiceUrl(this.url);
 
         final String[] transcript = new String[1];
+        transcript[0] = "";
+
         final Boolean[] transcriptionEnded = new Boolean[1];
         transcriptionEnded[0] = false;
 
         RecognizeOptions recognizeOptions = new RecognizeOptions.Builder()
                 .audio(new FileInputStream(getItem(position).getFilePath()))
-                .contentType("audio/mp3")
+                .contentType("audio/wav")
                 .model("en-US_BroadbandModel")
                 .maxAlternatives(5)
                 .build();
@@ -209,23 +221,27 @@ public class FileViewerAdapter extends RecyclerView.Adapter<FileViewerAdapter.Re
 
             @Override
             public void onTranscription (SpeechRecognitionResults speechRecognitionResults) {
-                List<SpeechRecognitionAlternative> alternatives = speechRecognitionResults.getResults().get(0).getAlternatives();
+                List<SpeechRecognitionResult> results = speechRecognitionResults.getResults();
 
-                int biggerConfidenceIndex = 0;
-                double maxConfidence = 0;
-                for (int i = 0; i < alternatives.size(); i++){
-                    double currentConfidence;
+                for (int resultsIndex = 0; resultsIndex < results.size(); resultsIndex++){
+                    List<SpeechRecognitionAlternative> alternatives = results.get(resultsIndex).getAlternatives();
 
-                    if (alternatives.get(i).getConfidence() != null) currentConfidence = alternatives.get(i).getConfidence();
-                    else currentConfidence = 0;
+                    int biggerConfidenceIndex = 0;
+                    double maxConfidence = 0;
+                    for (int alternativeIndex = 0; alternativeIndex < alternatives.size(); alternativeIndex++){
+                        double currentConfidence;
 
-                    if(currentConfidence > maxConfidence){
-                        maxConfidence = currentConfidence;
-                        biggerConfidenceIndex = i;
+                        if (alternatives.get(alternativeIndex).getConfidence() != null) currentConfidence = alternatives.get(alternativeIndex).getConfidence();
+                        else currentConfidence = 0;
+
+                        if(currentConfidence > maxConfidence){
+                            maxConfidence = currentConfidence;
+                            biggerConfidenceIndex = alternativeIndex;
+                        }
                     }
-                }
 
-                transcript[0] = alternatives.get(biggerConfidenceIndex).getTranscript();
+                    transcript[0] += alternatives.get(biggerConfidenceIndex).getTranscript() + " ";
+                }
             }
 
             @Override
@@ -312,20 +328,30 @@ public class FileViewerAdapter extends RecyclerView.Adapter<FileViewerAdapter.Re
         //user deletes a saved recording out of the application through another application
     }
 
+    private boolean newNameAlreadyExists(String newName){
+        boolean nameAlreadyExsists = false;
+
+        for (int i = 0; i < this.mDatabase.getCount() && !nameAlreadyExsists; i++){
+            if (newName.equals(this.mDatabase.getItemAt(i).getName())) nameAlreadyExsists = true;
+        }
+
+        return nameAlreadyExsists;
+    }
+
     public void rename(int position, String name) {
         //rename a file
-
         String mFilePath = Environment.getExternalStorageDirectory().getAbsolutePath();
         mFilePath += "/SoundRecorder/" + name;
         File f = new File(mFilePath);
 
         if (f.exists() && !f.isDirectory()) {
             //file name is not unique, cannot rename file.
-            Toast.makeText(mContext,
-                    String.format(mContext.getString(R.string.toast_file_exists), name),
-                    Toast.LENGTH_SHORT).show();
-
-        } else {
+            Toast.makeText(mContext, String.format(mContext.getString(R.string.toast_file_exists), name), Toast.LENGTH_SHORT).show();
+        }
+        else if (newNameAlreadyExists(name)){
+            Toast.makeText(mContext, mContext.getResources().getString(R.string.nameAlreadyExistsInDB), Toast.LENGTH_SHORT).show();
+        }
+        else {
             //file name is unique, rename file
             File oldFilePath = new File(getItem(position).getFilePath());
             oldFilePath.renameTo(f);
@@ -336,11 +362,12 @@ public class FileViewerAdapter extends RecyclerView.Adapter<FileViewerAdapter.Re
 
     public void shareFileDialog(int position) throws FileNotFoundException{
         File file = new File(getItem(position).getFilePath());
+
         if(file.exists()){
             Intent shareIntent = new Intent();
             shareIntent.setAction(Intent.ACTION_SEND);
             shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(getItem(position).getFilePath())));
-            shareIntent.setType("audio/mp3");
+            shareIntent.setType("audio/wav");
             mContext.startActivity(Intent.createChooser(shareIntent, mContext.getText(R.string.send_to)));
         }
         else{
@@ -348,43 +375,49 @@ public class FileViewerAdapter extends RecyclerView.Adapter<FileViewerAdapter.Re
         }
     }
 
-    public void renameFileDialog (final int position) {
-        // File rename dialog
-        AlertDialog.Builder renameFileBuilder = new AlertDialog.Builder(mContext);
+    public void renameFileDialog (final int position) throws FileNotFoundException{
+        File file = new File(getItem(position).getFilePath());
 
-        LayoutInflater inflater = LayoutInflater.from(mContext);
-        View view = inflater.inflate(R.layout.dialog_rename_file, null);
+        if (file.exists()){
+            // File rename dialog
+            AlertDialog.Builder renameFileBuilder = new AlertDialog.Builder(mContext);
 
-        final EditText input = (EditText) view.findViewById(R.id.new_name);
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            View view = inflater.inflate(R.layout.dialog_rename_file, null);
 
-        renameFileBuilder.setTitle(mContext.getString(R.string.dialog_title_rename));
-        renameFileBuilder.setCancelable(true);
-        renameFileBuilder.setPositiveButton(mContext.getString(R.string.dialog_action_ok),
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int id) {
-                        try {
-                            String value = input.getText().toString().trim() + ".mp3";
-                            rename(position, value);
+            final EditText input = (EditText) view.findViewById(R.id.new_name);
+            input.setText(file.getName().replace(".wav", ""));
 
-                        } catch (Exception e) {
-                            Log.e(LOG_TAG, "exception", e);
+            renameFileBuilder.setTitle(mContext.getString(R.string.dialog_title_rename));
+            renameFileBuilder.setCancelable(true);
+            renameFileBuilder.setPositiveButton(mContext.getString(R.string.dialog_action_ok),
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            try {
+                                String value = input.getText().toString().trim() + ".wav";
+                                rename(position, value);
+
+                            } catch (Exception e) {
+                                Log.e(LOG_TAG, "exception", e);
+                            }
+
+                            dialog.cancel();
                         }
+                    });
+            renameFileBuilder.setNegativeButton(mContext.getString(R.string.dialog_action_cancel),
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
 
-                        dialog.cancel();
-                    }
-                });
-        renameFileBuilder.setNegativeButton(mContext.getString(R.string.dialog_action_cancel),
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
-                    }
-                });
-
-        renameFileBuilder.setView(view);
-        AlertDialog alert = renameFileBuilder.create();
-        alert.show();
+            renameFileBuilder.setView(view);
+            AlertDialog alert = renameFileBuilder.create();
+            alert.show();
+        }
+        else throw new FileNotFoundException();
     }
 
     public void deleteFileDialog (final int position) {
