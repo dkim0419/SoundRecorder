11f8ad8c920b9b297818e115ea42c31d09396cd1
DESKTOP-K0NGA5M\Assunta De Caro <decaroassunta98@gmail.com>
2020-12-17 13:17:29 +0100

    Caching in travis gives problem

---
+++ app/src/androidTest/java/com/assuntadc/soundrecorder/integrationtest/FileViewerAdapterTester.java
@@ -0,0 +1,9 @@
+package com.assuntadc.soundrecorder.integrationtest;
+//ALL-CALL-SITE CRITERIA ADOPTED
+
+public class FileViewerAdapterTester{
+
+    //INTEGRATION TEST WITH CUSTOM ALERT DIALOG FOR EXTRACTED TEST
+
+
+}
